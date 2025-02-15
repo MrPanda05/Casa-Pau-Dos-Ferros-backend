af3da4f43d377afc90ac3d8edf76f2e91b75b3f4
Augusto <ammguto@gmail.com>
2025-02-15 00:35:20 -0300

    [Endereço] Adicionado Endereços do usuário Permite cadastro, consulta, e edição das informações de endereço do usuário

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("register/", user_register),
     path("login/", user_login),
     path("logout/", user_logout),
-    #path("profile/", user_profile),
+    # path("profile/", user_profile),
     #path("image/", user_image_update)
 ]
--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import *
+
+# address_list = AddressViewSet.as_view({
+#     'get': 'list'
+# })
+# address_detail = AddressViewSet.as_view({
+#     'get': 'retrieve',
+# })
+
+router = DefaultRouter()
+router.register(r'address', AddressViewSet, basename='address')
+router.register(r'users', UserViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("api.urls"))
+    path("api/", include("api.urls")),
+    path("", include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
 ]
