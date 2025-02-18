a37edfcd45b30a69141d959736d483cf38a6dea0
Augusto <ammguto@gmail.com>
2025-02-18 18:45:04 -0300

    [Product] Added product model, serializer and view

--- api/models.py
+++ api/models.py
@@ -24,3 +24,16 @@ class user_address(models.Model):
 
     def __str__(self):
         return self.address_id
+    
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, null=False)
+    description = models.TextField(null=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    image = models.ImageField(upload_to='media/product/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product_id
--- backend/pagination.py
+++ backend/pagination.py
@@ -4,3 +4,8 @@ class StandardPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+class ProductPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 100
